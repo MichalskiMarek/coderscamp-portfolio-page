bfa72253e3f229b6f3fbd44c8b54a88ee11cb3eb
Marek Michalski <michalskimarek89@gmail.com>
2019-11-04 08:00:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".menu__container");
-console.log(document);
 
 const handleClick = () => {
   hamburger.classList.toggle("hamburger--active");
